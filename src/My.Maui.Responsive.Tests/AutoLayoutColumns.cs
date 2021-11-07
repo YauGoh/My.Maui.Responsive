f41bb0f52bca74011acb634f20ab78e87659e985
Yau Goh Chow <yaugoh.chow@gmail.com>
2021-11-07 21:01:15 +0800

    Initial work for tests

--- src/My.Maui.Responsive.Tests/AutoLayoutColumns.cs
+++ src/My.Maui.Responsive.Tests/AutoLayoutColumns.cs
@@ -2,7 +2,7 @@ using Xunit;
 
 namespace My.Maui.Responsive.Tests;
 
-public class UnitTest1
+public class AutoLayoutColumns
 {
     [Fact]
     public void Test1()
---
+++ src/My.Maui.Responsive.Tests/MockViewBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Graphics;
+using Moq;
+
+namespace My.Maui.Responsive.Tests
+{
+    internal class MockViewBuilder
+    {
+        private readonly double _requiredHeight;
+
+        internal static MockViewBuilder Default => new MockViewBuilder(20);
+
+        private MockViewBuilder(double requiredHeight)
+        {
+            _requiredHeight = requiredHeight;
+        }
+
+        public MockViewBuilder WithHeight(double height) => new MockViewBuilder(height);
+
+        internal Mock<IView> Build()
+        {
+            var mock = new Mock<IView>();
+
+            mock.Setup(_ => _.Measure(It.IsAny<double>(), It.IsAny<double>())).Returns((double width, double height) => new Size(width, 20.0));
+
+            return mock;
+        }
+    }
+}
